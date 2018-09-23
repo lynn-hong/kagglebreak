4388213cb29d6a8bcea9ef9f805a5974bc50a331
lynn-hong <lynnn.hong@gmail.com>
2018-09-23 10:56:08 +0900

    screen height을 체크하는 부분을 수정(#11)

--- website/static/databreak2018/js/main.js
+++ website/static/databreak2018/js/main.js
@@ -3,13 +3,13 @@ $(document).ready(function(){
 	"use strict";
 
 	var window_width 	 = $(window).width(),
-	window_height 		 = window.innerHeight,
+	window_height 		 = $(window).height(),
 	header_height 		 = $(".default-header").height(),
 	header_height_static = $(".site-header.static").outerHeight(),
 	fitscreen 			 = window_height - header_height;
 
 
-	$(".fullscreen").css("height", window_height)
+	$(".fullscreen").css("height", window_height);
 	$(".fitscreen").css("height", fitscreen);
 
      if(document.getElementById("default-select")){

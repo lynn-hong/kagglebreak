76b37f1d4d5f91a7466d1fbc8fbffcf8d91b2400
lynn-hong <lynnn.hong@gmail.com>
2018-09-29 00:24:33 +0900

    행사 시작 시간을 오전 10시로 변경(#22)

--- website/static/databreak2018/js/main.js
+++ website/static/databreak2018/js/main.js
@@ -225,7 +225,7 @@ $(document).ready(function(){
 
 
     // Set the date we're counting down to
-    var countDownDate = new Date("Oct 7, 2018 11:00:00").getTime();
+    var countDownDate = new Date("Oct 7, 2018 10:00:00").getTime();
 
     // Update the count down every 1 second
     var x = setInterval(function() {

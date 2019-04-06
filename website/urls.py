5586d42d614905c7fcf78654dae329b5bb0da280
lynn-hong <lynnn.hong@gmail.com>
2019-04-07 00:05:42 +0900

    스폰서십 페이지 추가, 리스트 디자인 요소 추가(#36)

--- website/urls.py
+++ website/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     url(r'^activity/$', views.IndexActivity.as_view(), name='activity'),
     url(r'^activity/(?P<a_id>\d+)/$', views.each_activity, name="each_activity"),
     url(r'^archive/$', views.IndexArchive.as_view(), name='archive'),
+    url(r'^sponsor/$', views.IndexSponsor.as_view(), name="sponsor"),
     url(r'^databreak2018/$', views.IndexDatabreak2018.as_view(), name='databreak2018'),
     url(r'^databreak2018/coc/$', views.IndexCoC.as_view(), name="coc"),
     url(r'^databreak2018/speech/(?P<pk>\d+)/$', views.speech, name="speech"),
